596e229a1d9b261883b2c3e7fa69b9eeef6f91bb
Patrick Gray <patrickgray@Patricks-MacBook-Pro.local>
2018-06-18 20:52:24 -0600

    2nd commit - created some data structures and accessed some data.

--- js/answer.js
+++ js/answer.js
@@ -20,3 +20,22 @@
 // A list of student names from our class, each with a location and each with a list of favorite tv shows. 
 // an object would be ideal because each student could be stored along with their location  and tv shows easily.
 
+// Take it Easy
+// Make an array that holds all of the colors of the rainbow.
+
+const royGBiv = ['red','orange','yellow','green','blue','indigo','violet'];
+
+// Write code that will access "blue" from the rainbow array.
+
+console.log(royGBiv[4]); 
+
+// Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+
+const patrickGray = {
+	favoriteFood : 'pizza',
+	hobby : 'video game',
+	location : 'Denver',
+	datatype: 'object'
+}
+
+// Write code that will access your hobby from the object that you just created.
