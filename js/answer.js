3bcc80466848d23ce2de3cf0b7e02c8e6dfd903d
Patrick Gray <patrickgray@Patricks-MacBook-Pro.local>
2018-06-18 20:42:32 -0600

    1st commit - selecting datatypes.

---
+++ js/answer.js
@@ -0,0 +1,22 @@
+//1st commit
+// Answer the Following
+// For each of the following, write which datatypes you would use to represent the data, and then give a small example of the data structure:
+
+// // A light switch that can be either on or off.
+// Boolean, because it can be true or false
+
+// // A user's email address.
+// a string so the user can save all of their information without causing any conflics.
+
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+//object so that the properties of the ship can be saved.
+
+// A list of student names from our class.
+// array because it's a series of strings.
+
+// A list of student names from our class, each with a location.
+//an object or a nested array because the location information can be saved as a key and value in an object, but since it's not a lot of detail, it could be stored as a nested array for each individual. [Patrick, Denver][Donnatello, NYC]
+
+// A list of student names from our class, each with a location and each with a list of favorite tv shows. 
+// an object would be ideal because each student could be stored along with their location  and tv shows easily.
+
